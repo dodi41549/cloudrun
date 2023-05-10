7d1b6864c10756766b6bf76c4882874f0fcba38b
dodi41549 <101625141+dodi41549@users.noreply.github.com>
2023-05-10 16:48:00 +0700

    Update server.js

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const http = require('http')
 const server = http.createServer((req, res) => {
     console.log('New connection')
-    res.end('Hello DEV this is production')
+    res.end('Hello DEV this is production for server Developer.')
 })
 const PORT = process.env.PORT || 8080
 server.listen(PORT, () => console.log('Listening'))
