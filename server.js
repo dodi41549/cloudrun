bb83982d2b47328f7245c0d681afc7c1b0dcc4ce
dodi41549 <101625141+dodi41549@users.noreply.github.com>
2023-05-10 17:29:06 +0700

    Update server.js

--- server.js
+++ server.js
@@ -1,7 +1,11 @@
-const http = require('http')
+const http = require('http');
+
 const server = http.createServer((req, res) => {
-    console.log('New connection')
-    res.end('Hello DEV this is production for server Developer.')
-})
-const PORT = process.env.PORT || 8080
-server.listen(PORT, () => console.log('Listening'))
+    console.log('New connection');
+    res.writeHead(200, { 'Content-Type': 'text/html' });
+    res.write('<html><body><p style="font-size: 32px;">Hello for EVERYONE this is a server names deployment for our users.^<>^</p></body></html>');
+    res.end();
+});
+
+const PORT = process.env.PORT || 8080;
+server.listen(PORT, () => console.log('Listening'));
